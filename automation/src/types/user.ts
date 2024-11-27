9e50dcc7398ef0a9ea8de932273261aa90586595
rkeane1996 <rkeane1996@users.noreply.github.com>
2024-11-27 13:12:01 +0000

    deploy: 4a8a2b511fd282e013dce1c39f39a6ba798ab124

--- automation/src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export type User = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  age: number;
-  salary: number;
-  department: string;
-  [key: string]: any;
-};
--- automation/src/util/assertions.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from '@playwright/test';
-
-export async function isTrue(condition: boolean) {
-  await expect(condition).toBeTruthy();
-}
-
-export async function isFalse(condition: boolean) {
-  await expect(condition).toBeFalsy();
-}
